5c731bfd8e867974d06db9aa2b26d2aecfcdf1d0
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-07-05 05:27:52 +0100

    Add mlp

---
+++ micrograd/engine/mlp.py
@@ -0,0 +1,19 @@
+from micrograd.engine.layer import Layer
+
+
+class mlp:
+
+    def __init__(self, n_inputs, n_outputs):
+        assert (
+            isinstance(n_inputs, int) and n_inputs > 0
+        ), "n_inputs must be a positive integer"
+        assert (
+            isinstance(n_outputs, list) and len(n_outputs) > 0
+        ), "n_outputs must be a positive integer"
+        sizes = [n_inputs] + n_outputs
+        self.layers = [Layer(sizes[i], sizes[i + 1]) for i in range(len(sizes) - 1)]
+
+    def __call__(self, inputs):
+        for layer in self.layers:
+            inputs = layer(inputs)
+        return inputs
