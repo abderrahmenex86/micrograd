504122768d05b6074facbaf20a611a63b8a7eab2
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-07-05 05:27:41 +0100

    Add layer

---
+++ micrograd/engine/layer.py
@@ -0,0 +1,11 @@
+from micrograd.engine.neuron import Neuron
+
+
+class Layer:
+
+    def __init__(self, n_inputs, n_outputs):
+        self.neurons = [Neuron(n_inputs) for _ in range(n_outputs)]
+
+    def __call__(self, inputs):
+        outs = [neuron(inputs) for neuron in self.neurons]
+        return outs
